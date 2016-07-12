da5cb79cb8f1512fb19fb8dbf535073faa2bc952
Irina <izAppleApex@optimisticlock.com>
2016-07-12 00:28:14 -0700

    ok merged

--- server/main.js
+++ server/main.js
@@ -25,6 +25,33 @@ Meteor.methods({
             throw new Meteor.Error(e)
         })
 
-        throw new Meteor.Error(500, 'Error 500: Not found', 'the document is not found');
+        console.time("getPage")
+
+ //       var p = getPage('http://belmarstore.com/',
+
+        var p = getPage(url,
+            function resolve(values) {
+                console.log("Values: " + values)
+                console.timeEnd("getPage")
+                // Insert a post into the collection
+                Sites.insert({
+                    url,
+                    createdAt: new Date(),            // current time
+       //             author: Meteor.userId(),           // _id of logged in user
+       //             lat: latLng.lat,
+       //             lng: latLng.lng,
+       //             username: username  // username of logged in user
+                      values
+                });
+            },
+
+            function reject(error) {
+                console.log("Error", error)
+                var e = new Meteor.Error("Server error")
+                e.old = error
+                throw new Meteor.Error(500, 'Error 500: Not found', 'the document is not found');
+
+            })
+
     }
 });
