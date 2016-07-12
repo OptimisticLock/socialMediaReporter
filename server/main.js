717c805ab9ae06f39528fed026f1316c9d041f4e
Irina <izAppleApex@optimisticlock.com>
2016-07-11 23:58:59 -0700

    ok, alert

--- server/main.js
+++ server/main.js
@@ -1,4 +1,4 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import { log, getPage } from '/imports/somethingSel'
 
 Meteor.startup(() => {
@@ -6,36 +6,7 @@ Meteor.startup(() => {
 });
 
 Meteor.methods({
-    addUrl({ url, arg2 }) {
-        log("kuku!", url)
-
-        console.time("getPage")
-
- //       var p = getPage('http://belmarstore.com/',
-
-        var p = getPage(url,
-            function resolve(values) {
-                console.log("Values: " + values)
-                console.timeEnd("getPage")
-                // Insert a post into the collection
-                Sites.insert({
-                    url,
-                    createdAt: new Date(),            // current time
-       //             author: Meteor.userId(),           // _id of logged in user
-       //             lat: latLng.lat,
-       //             lng: latLng.lng,
-       //             username: username  // username of logged in user
-                      values
-                });
-            },
-
-            function reject(error) {
-                console.log("Error", error)
-                var e = new Meteor.Error("Server error")
-                e.old = error
-                throw new Meteor.Error(500, 'Error 500: Not found', 'the document is not found');
-
-            })
-
+    addUrl({url, arg2}) {
+        throw new Meteor.Error(500, 'Error 500: Not found', 'the document is not found');
     }
 });
