210d1f0fd51a74ae1baf1403a6561a4bdfd64640
Irina <izAppleApex@optimisticlock.com>
2016-07-12 00:22:24 -0700

    ok alert still

--- server/main.js
+++ server/main.js
@@ -5,8 +5,26 @@ Meteor.startup(() => {
 
 });
 
+function timesByTwo(n, delay) {
+    return new Promise((resolve, reject) => {
+        Meteor.setTimeout(() => {
+            if (typeof n !== 'number') {
+                console.log("rejecting")
+                reject('Not-a-number')
+            }
+            resolve(n * 2)
+        }, delay)
+    })
+}
+
+
 Meteor.methods({
     addUrl({url, arg2}) {
+        console.log("add url")
+        return timesByTwo("kuku", 1000).catch(e => {
+            throw new Meteor.Error(e)
+        })
+
         throw new Meteor.Error(500, 'Error 500: Not found', 'the document is not found');
     }
 });
